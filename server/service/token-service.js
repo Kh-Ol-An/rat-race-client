0ea8962815a26c3579e3ed09f9f09006425b8b13
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-08 21:59:42 +0200

    Added refreshToken to create token model

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -3,7 +3,7 @@ const tokenModel = require('../models/token-model');
 
 class TokenService {
     generatesTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '1d' });
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '4h' });
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
         return {
             accessToken,
@@ -18,7 +18,7 @@ class TokenService {
             return foundToken.save();
         }
 
-        return await tokenModel.create({ user: userId });
+        return await tokenModel.create({ user: userId, refreshToken });
     }
 }
 
