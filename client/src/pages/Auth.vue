9439d2184e7f9048cae9190f201e3beee2da155a
Oleg Khristenk <olegkhristenko@gmail.com>
2023-01-14 22:39:54 +0200

    Created Auth

---
+++ client/src/pages/Auth.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full h-screen flex items-center justify-center">
+        <div class="py-6 px-8 flex flex-col items-center justify-center gap-4 shadow-lg rounded-md bg-slate-800">
+            <Input v-model:value="email" type="text" id="email" placeholder="Пошта" />
+            <Input v-model:value="password" type="text" id="password" placeholder="Пароль" />
+            <Input v-model:value="repeatPassword" type="text" id="repeat-password" placeholder="Повторити пароль" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Input from '../components/Blank/plugins/Input.vue';
+
+const email = ref('');
+const password = ref('');
+const repeatPassword = ref('');
+
+console.log(email.value, password.value);
+</script>
--- client/src/router.js
+++ client/src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Blank from './pages/Blank.vue';
 import Game from './pages/Game.vue';
+import Auth from './pages/Auth.vue';
 
 export default createRouter({
     history: createWebHashHistory(),
     routes: [
         { path: '/', component: Blank },
         { path: '/game', component: Game },
+        { path: '/auth', component: Auth },
     ],
 });
