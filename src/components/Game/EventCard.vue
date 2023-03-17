ee7b5d397de2e79ee8f187f2bd3e0beaebc6010a
Oleg Khristenk <olegkhristenko@gmail.com>
2023-03-17 17:12:59 +0200

    Added possible to choose business

--- src/components/Game/EventCard.vue
+++ src/components/Game/EventCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref, watchEffect } from 'vue'
+import { computed, ref } from 'vue'
 import eventCards from '../../database/event-cards.json'
 import randomInteger from '../../helpers/random-integer.js'
 
@@ -20,30 +20,32 @@ const props = defineProps({
 
 const emit = defineEmits(['confirm', 'cancel'])
 
-const eventCard = ref(props.userPositionOnFields)
+const chosenBusinessType = ref('')
 
-watchEffect(() => {
+const eventCard = computed(() => {
     const rand = randomInteger(1, 3)
-    // console.log('rand: ', rand)
-    // console.log("props.userPositionOnFields.type: ", props.userPositionOnFields.type)
-    // console.log("eventCards[props.userPositionOnFields.type]: ", eventCards[props.userPositionOnFields.type])
+    if (props.userPositionOnFields.type === 'business') {
+        return chosenBusinessType.value.length
+            ? eventCards.business[chosenBusinessType.value].find((card) => card.id === rand)
+            : props.userPositionOnFields
+    }
+
     if (
-        props.userPositionOnFields.type === 'business' ||
         props.userPositionOnFields.type === 'investments' ||
         props.userPositionOnFields.type === 'expenses' ||
         props.userPositionOnFields.type === 'opportunities' ||
         props.userPositionOnFields.type === 'buys'
     ) {
-        eventCard.value = eventCards[props.userPositionOnFields.type].find(
-            (card) => card.id === rand
-        )
+        return eventCards[props.userPositionOnFields.type].find((card) => card.id === rand)
     }
 
-    // console.log('eventCard: ', eventCard.value)
-    // console.log('****************')
+    return props.userPositionOnFields
 })
 
-const confirm = () => emit('confirm', eventCard.value)
+const confirm = () => {
+    emit('confirm', eventCard.value)
+    chosenBusinessType.value = ''
+}
 
 const isCancel = computed(
     () =>
@@ -56,7 +58,6 @@ const isCancel = computed(
 
 <template>
     <div
-        v-if="eventCard"
         :style="{ width, height }"
         :class="[
             'absolute',
@@ -68,40 +69,65 @@ const isCancel = computed(
             userPositionOnFields.styles.bg,
         ]"
     >
-        <h4 v-if="eventCard.name" class="text-center text-2xl font-bold text-slate-400">
-            {{ eventCard.name }}
-        </h4>
-        <p v-if="eventCard.description" class="text-center text-slate-400">
-            {{ eventCard.description }}
-        </p>
-        <span
-            v-if="eventCard.profit"
-            class="text-center text-xl font-bold text-slate-400"
+        <div
+            v-if="userPositionOnFields.type === 'business' && chosenBusinessType.length === 0"
+            class="absolute top-0 right-0 bottom-0 left-0 w-full h-full flex flex-col items-center justify-center"
         >
-            {{ eventCard.profit }}
-        </span>
-        <span
-            v-if="eventCard.price"
-            class="text-center text-xl font-bold text-slate-400"
-        >
-            {{ eventCard.price }}
-        </span>
-        <div :class="['grid', isCancel && 'grid-cols-2 gap-3']">
             <button
-                class="rounded bg-gradient-to-b from-oppositeLight to-opposite px-4 py-3 text-xl font-bold text-slate-200 shadow-[0_5px_15px_rgba(0,0,0,0.35)] outline-0"
+                class="w-full h-1/2 flex items-center justify-center bg-gradient-to-b from-primaryLight to-primary text-center font-bold text-6xl text-slate-300"
                 type="button"
-                @click="confirm"
+                title="Малий бізнес"
+                @click="chosenBusinessType = 'small'"
             >
-                {{ userPositionOnFields.action }}
+                Малий бізнес
             </button>
             <button
-                v-if="isCancel"
-                class="rounded bg-gradient-to-b from-primaryLight to-primary px-4 py-3 text-xl font-bold text-slate-200 shadow-[0_5px_15px_rgba(0,0,0,0.35)] outline-0"
+                class="w-full h-1/2 flex items-center justify-center bg-gradient-to-b from-secondaryLight to-secondary text-center font-bold text-6xl text-slate-300"
                 type="button"
-                @click="$emit('cancel')"
+                title="Великий бізнес"
+                @click="chosenBusinessType = 'big'"
             >
-                Заощадити
+                Великий бізнес
             </button>
+        </div>
+
+        <div v-if="eventCard" class="w-full h-full flex flex-col items-center justify-around">
+            <h4 v-if="eventCard.name" class="text-center text-2xl font-bold text-slate-400">
+                {{ eventCard.name }}
+            </h4>
+            <p v-if="eventCard.description" class="text-center text-slate-400">
+                {{ eventCard.description }}
+            </p>
+            <span
+                v-if="eventCard.profit"
+                class="text-center text-xl font-bold text-slate-400"
+            >
+                {{ eventCard.profit }}
+            </span>
+            <span
+                v-if="eventCard.price"
+                class="text-center text-xl font-bold text-slate-400"
+            >
+                {{ eventCard.price }}
+            </span>
+
+            <div :class="['grid', isCancel && 'grid-cols-2 gap-3']">
+                <button
+                    class="rounded bg-gradient-to-b from-oppositeLight to-opposite px-4 py-3 text-xl font-bold text-slate-200 shadow-[0_5px_15px_rgba(0,0,0,0.35)] outline-0"
+                    type="button"
+                    @click="confirm"
+                >
+                    {{ userPositionOnFields.action }}
+                </button>
+                <button
+                    v-if="isCancel"
+                    class="rounded bg-gradient-to-b from-primaryLight to-primary px-4 py-3 text-xl font-bold text-slate-200 shadow-[0_5px_15px_rgba(0,0,0,0.35)] outline-0"
+                    type="button"
+                    @click="$emit('cancel')"
+                >
+                    Заощадити
+                </button>
+            </div>
         </div>
     </div>
 </template>
