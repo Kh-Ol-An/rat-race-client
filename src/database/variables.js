41c4bf67d4d804db114d3d8bc42ffd14904237b2
kholan <olegkhristenko@gmail.com>
2023-03-18 11:28:44 +0200

    Deleted profit field

--- src/database/variables.js
+++ src/database/variables.js
@@ -1,7 +1,7 @@
 export const CIRCLE_FACTOR = 0.526315789
-export const CELLS_COUNT_BY_WIDTH = 19
-export const CELLS_COUNT_BY_HEIGHT = 10
-export const FIELDS_COUNT = 54
+export const CELLS_COUNT_BY_WIDTH = 16
+export const CELLS_COUNT_BY_HEIGHT = 9
+export const FIELDS_COUNT = 48
 export const BLANK_X_GAP = 38
 export const BLANK_Y_GAP = 24
 export const BLANK_FACTOR = 0.65
